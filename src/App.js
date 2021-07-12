c5c617b37df9d6761dbefc2ea8da4eb73565be88
SerhioAndros <androsiuksv@gmail.com>
2021-07-12 14:44:27 +0300

    basic

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div className="App">Hello World!</div>;
+  }
+}
+
+export default App;
