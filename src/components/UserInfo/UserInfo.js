95598876ea85cb4bd3f898df54436d0746cfbb7a
SerhioAndros <androsiuksv@gmail.com>
2021-07-13 16:10:30 +0300

    merge corrections

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,30 +1,26 @@
 import React, { useCallback } from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import { authOperations, authSelectors } from "../../redux/auth";
+import { useSelector, useDispatch } from "react-redux";
+import { getAuthUserName } from "../../redux/auth/auth-selectors";
+import { logOut } from "../../redux/auth/auth-operation";
+
 import css from "./UserInfo.module.css";
 import Line from "../../images/line.png";
 
-export default function UserInfo() {
+export function UserInfo() {
   const dispatch = useDispatch();
-  const name = useSelector(authSelectors.getUserName);
+  const name = useSelector(getAuthUserName);
   const onLogOut = useCallback(() => {
-    dispatch(authOperations.logOut());
+    dispatch(logOut());
   }, [dispatch]);
-
-  <div className={css.userContainer}>
-    <div className={css.userInfo}>
-      <span className={css.name}>{name}</span>
-      <img
-        className={css.Line}
-        src={Line}
-        alt="line"
-        width="2"
-        height="32"
-      />
-      <button type="button" className={css.logout} onClick={onLogOut}>
-        Выйти
-      </button>
+  return (
+    <div className={css.userContainer}>
+      <div className={css.userInfo}>
+        <span className={css.name}>{name}</span>
+        <img className={css.Line} src={Line} alt="line" width="2" height="32" />
+        <button type="button" className={css.logout} onClick={onLogOut}>
+          Выйти
+        </button>
+      </div>
     </div>
-  </div>
-};
-
+  );
+}
