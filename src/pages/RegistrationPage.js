e4b2f67292c50d7cdfa78477db00b9f2d0169b45
SerhioAndros <androsiuksv@gmail.com>
2021-07-13 13:17:19 +0300

    test commit
    test

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import RegistrationForm from "../components/registerForm/RegistrationForm";
 
 const RegistrationPage = () => {
-  return <h1>Registration</h1>;
+  return (
+    <div>
+      <h1>РЕГИСТРАЦИЯ</h1>
+      <RegistrationForm />
+    </div>
+  );
 };
 
 export default RegistrationPage;
