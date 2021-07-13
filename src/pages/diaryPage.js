ae431b2251d5ca781605f7cd29b41c311def826f
Svitlana Burba <svitlana.burba@gmail.com>
2021-07-13 15:56:27 +0200

    capital letters changed one more time

--- src/pages/diaryPage.js
+++ src/pages/diaryPage.js
@@ -1,12 +1,12 @@
-import React from "react";
-import DiaryDateCalendar from "../components/diary/diaryDateCalendar/DiaryDateCalendar";
-import DiaryAddProductForm from "../components/diary/diaryAddProductForm/DiaryAddProductForm";
-import DiaryProductsList from "../components/diary/diaryProductsList/DiaryProductsList";
+import React from 'react';
+import DiaryDateCalendar from '../components/diary/diaryDateCalendar/DiaryDateCalendar';
+import DiaryAddProductForm from '../components/diary/diaryAddProductForm/DiaryAddProductForm';
+import DiaryProductsList from '../components/diary/diaryProductsList/DiaryProductsList';
 import {
   DiaryPageContainer,
-  DiaryPageInnerContainer,
-} from "./DiaryPage.module.css";
-import Sidebar from "../components/diary/Sidebar";
+  DiaryPageInnerContainer
+} from './DiaryPage.module.css';
+import RightSideBar from '../components/rightSideBar/RightSideBar';
 
 export default function DiaryPage() {
   return (
@@ -16,7 +16,7 @@ export default function DiaryPage() {
         <DiaryAddProductForm />
         <DiaryProductsList />
       </div>
-      <Sidebar />
+      <RightSideBar />
     </div>
   );
 }
