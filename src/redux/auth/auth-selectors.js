a8d7a4f0574fc2ca1aa2528f15a889f77e73b04e
Maksym-1989 <zaksumy1989@gmail.com>
2021-07-14 18:07:19 +0300

    done

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,6 +1,7 @@
 const getIsAuth = state => state.auth.isAuthenticated;
 const getAuthUserName = state => state.auth.user.username;
+const getIsRegistr = state => state.auth.isRegistrated;
 
 const getAuthToken = state => state.auth.token;
 
-export {getIsAuth, getAuthUserName, getAuthToken};
+export {getIsAuth, getAuthUserName, getAuthToken, getIsRegistr};
