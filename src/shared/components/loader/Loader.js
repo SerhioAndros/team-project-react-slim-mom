11838a8b8839d26064e222cc0f05d46517da03f2
SerhioAndros <androsiuksv@gmail.com>
2021-07-14 13:37:25 +0300

    spinner v2

--- src/shared/components/loader/Loader.js
+++ src/shared/components/loader/Loader.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Loader from "react-loader-spinner";
+import styles from "./Loader.module.css";
 
 export const AppLoader = () => {
   return (
-    <div className="wrapper">
+    <div className={styles.loaderWrapper}>
       <Loader
         type="Puff"
         color="#FC842D"
-        height={100}
-        width={100}
+        height={500}
+        width={500}
         timeout={3000}
       />
     </div>
