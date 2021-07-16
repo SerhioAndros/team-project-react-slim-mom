67d63bcca75a5daf1f523886923e3433a4147047
Svitlana Burba <svitlana.burba@gmail.com>
2021-07-16 14:05:20 +0200

    corrections

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,48 +1,50 @@
-import React, { useState } from "react";
-import DailyCalorieIntake from "../components/DailyCalorieIntake/DailyCalorieIntake";
-import MainForm from "../components/mainForm/MainForm";
-import Modal from "../components/Modal/Modal";
-import { getDailyRate } from "../services/apiService/getDailyRate";
-import Container from "../components/Container/Container";
-import Section from "../components/Section/Section";
+import React, {useState} from 'react';
+import DailyCalorieIntake from '../components/DailyCalorieIntake/DailyCalorieIntake';
+import MainForm from '../components/mainForm/MainForm';
+import Modal from '../components/Modal/Modal';
+import {getDailyRate} from '../services/apiService/getDailyRate';
+import Container from '../components/Container/Container';
+import Section from '../components/Section/Section';
 
 const HomePage = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({dailyRate: '', notAllowedProducts: []});
   const [showModal, setShowModal] = useState(false);
-  const onSubmit = async (values) => {
+  const onSubmit = async values => {
     try {
       const result = await getDailyRate(values);
       // if (result) {
-        setShowModal(true);
-        setData(result.data)
+      setShowModal(true);
+      setData(result.data);
       // }
     } catch (error) {}
   };
   console.log(data);
   return (
     <>
-    <Container>
-      <Section>
-      <MainForm
-        onSubmit={onSubmit}
-        phraze={"Просчитай свою суточную норму калорий прямо сейчас"}
-        initialValues={{
-          height: "",
-          age: "",
-          weight: "",
-          desiredWeight: "",
-          bloodType: "1",
-        }}
-      />
-    </Section>
-    </Container>
+      <Container>
+        <Section>
+          <MainForm
+            onSubmit={onSubmit}
+            phraze={'Просчитай свою суточную норму калорий прямо сейчас'}
+            initialValues={{
+              height: '',
+              age: '',
+              weight: '',
+              desiredWeight: '',
+              bloodType: '1'
+            }}
+          />
+        </Section>
+      </Container>
       {showModal && (
         <Modal>
-          <DailyCalorieIntake calories={data.dailyRate}   products={data.notAllowedProducts}/>
+          <DailyCalorieIntake
+            calories={data.dailyRate}
+            products={data.notAllowedProducts}
+          />
         </Modal>
       )}
     </>
-
   );
 };
 
