2be221add84100297276eff9309862d72208e5d0
Maksym-1989 <zaksumy1989@gmail.com>
2021-07-16 09:57:55 +0300

    done

--- src/components/rightSideBar/RightSideBar.js
+++ src/components/rightSideBar/RightSideBar.js
@@ -13,7 +13,7 @@ const RightSideBar = () => {
   const selectedDate = useSelector(selectors.getSelectedDate);
 
   const dispatch = useDispatch();
-
+console.log(daySummary)
   useEffect(() => {
     dispatch(getUserDayInfo());
   }, [dispatch]);
