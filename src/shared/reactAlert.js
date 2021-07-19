2629af21e36d6525ec4969b3082f9795cf234361
Maksym-1989 <zaksumy1989@gmail.com>
2021-07-19 18:53:31 +0300

    done

---
+++ src/shared/reactAlert.js
@@ -0,0 +1,29 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function alertError(message) {
+  return toast.error(message, {
+    position: "top-right",
+    autoClose: 4000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    // transition: "zoom",
+  });
+}
+function alertSuccess(message) {
+  return toast.success(message, {
+    position: "top-right",
+    autoClose: 4000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    // transition: "zoom",
+  });
+}
+
+export { alertError, alertSuccess };
